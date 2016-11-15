dccb48ff8bce8d48bd6db08772d0e954eefa0839
Sukora-Stas <stasiksukora@mail.ru>
2016-11-16 01:33:12 +0300

    correction lab3

--- All_lab/lab1/lab1/lab1.cpp
+++ All_lab/lab1/lab1/lab1.cpp
@@ -3,34 +3,35 @@
 
 using namespace std;
 
-int â()
+int main()
 {
-	double x, y, s;
-	s = 0;
+	double x;
+	double y;
+	double s;
 	cout << "Vedite X: ";
 	cin >> x;
 	cout << "Vedite Y: ";
 	cin >> y;
-	if (x / y < 0){
-		s = 2 / 3 + log(abs(sin(y)));
-		cout << "Vetv2  " << endl;
-		cout << "Otvet: s=  " << s << endl;
+	if (y == 0){
+		cout << "Division by zero is impossible to enter another number" << endl;
+		cout << "Vedite Y: ";
+		cin >> y;
 	}
 	else if (x / y > 0){
 		s = pow(x + log(abs(y)), 3);
 		cout << "Vetv1  " << endl;
 		cout << "Otvet: s=  " << s << endl;
 	}
-	else if (x ==0 && y==0){
-		s = pow(x + log(abs(y)), 3);
-		cout << "It is impossible to divide by 0" << endl;
+	else if (x / y < 0){
+		s = 2 / 3 + log(abs(sin(y)));
+		cout << "Vetv2  " << endl;
+		cout << "Otvet: s=  " << s << endl;
 	}
 	else{
 		s = (pow(pow(x, 2), 1 / 3) + y);
 		cout << "Vetv3  " << endl;
 		cout << "Otvet: s=  " << s << endl;
 	}
-	
 	system("pause");
 	return 0;
 }
--- All_lab/lab3/lab3/lab3.cpp
+++ All_lab/lab3/lab3/lab3.cpp
@@ -1,74 +1,63 @@
 #include <iostream>
-#include<iomanip>
+#include <iomanip>
 #include <math.h>
 #include <stdio.h>
-
 using namespace std;
 
 void sredArifm(int a[], int, double*, int*, int*, int*);
-void MinMax(int a[], int, int*, int*, int*, int*);  // Прототип функции 
-const int nl = 10;
+
 int main()
 {
 	double sred;
-	int i, n, a[10], sum, max, min, imin, imax;;
+	int i, n, a[10], sum, max, min;
+
 	cout << "Vvedite razmer massiva: ";
 	cin >> n;
-	cout << "\Vvedite massiv:\n";
+	cout << "Vvedite massiv:\n";
 	for (i = 0; i<n; i++)
 	{
 		cout << "Vvedite a[" << i << "]=";
 		cin >> a[i];
 	}
-//cout << "\Massiv a:\n";
-	MinMax(a, n, &min, &max, &imin, &imax);
-	
+	cout << "Massiv a:\n";
+	for (i = 0; i<n; i++)
+		cout << setw(7) << a[i];
+	cout << endl;
+	max = a[0];
+	min = a[0];
 	sredArifm(a, n, &sred, &sum, &max, &min);
 	//cout << a[i] << endl;
-	cout << "\nPosPol= " << &max << "\nPervOtr= " << &min<<endl;
+	//cout << "\nPosPol= " << *max << "\nPervOtr= " << *min<<endl;
 	cout << "\nsred=" << sred << endl;
 	system("pause");
 	return 0;
 }
-void MinMax(int a[], int n, int *min, int *max, int *imin, int *imax)
-{
-	int i;
-	*min = *max = a[0];      // Инициализация значений 
-	*imin = *imax = 0;
-	for (i = 1; i < n; i++)
-		if (a[i]<*min) {    // Поиск минимального элемента и его индекса    
-			*min = a[i];
-			*imin = i;
-		}
-		else  if (a[i]>*max) { // Поиск максимального элемента и его индекса    
-			*max = a[i];
-			*imax = i;
-		}
-}
+
 void sredArifm(int a[], int n, double* sred, int*sum, int*posPol, int*pervOtr)
 {
 	int i;
+
 	posPol = 0;
 	pervOtr = 0;
+
 	for (i = 0; i < n; i++)
 	{
-		//printf("%x ", &a[i]);
-		//printf("%x ", a + i);
-		
+		printf("%x ", &a[i]);
+		printf("%x ", a + i);
+		printf("%d\n", a[i]);
 		if (a[i] < 0)
 		{
 			pervOtr = a + i;
-			//printf("pervOtr %x ", a + i);
-			//printf("%x\n", pervOtr);
+			printf("pervOtr %x ", a + i);
+			printf("%x\n", pervOtr);
 			break;
 		}
-		printf("test %d\n", a[i]);
 	}
 	for (i = n - 1; i >= 0; i--)
 	{
-		//printf("%x ", &a[i]);
-		//printf("%x ", a + i);
-		
+		printf("%x ", &a[i]);
+		printf("%x ", a + i);
+		printf("%d\n", a[i]);
 		if (!(a[i] % 2))
 		{
 			if (a[i] > 0)
@@ -79,10 +68,11 @@ void sredArifm(int a[], int n, double* sred, int*sum, int*posPol, int*pervOtr)
 				break;
 			}
 		}
-		printf("%d\n", a[i]);
 	}
+
 	if (posPol &&  pervOtr)
 	{
+
 		int n;
 		n = 0;
 		*sum = 0;
@@ -97,8 +87,13 @@ void sredArifm(int a[], int n, double* sred, int*sum, int*posPol, int*pervOtr)
 		}
 		printf("n: %d \n", n);
 
-	printf("sum: %d \n", *sum);
+		printf("sum: %d \n", *sum);
+
 		*sred = (double)*sum / (double)n;
+
 		printf("sred: %f \n", *sred);
+
 	}
+
+
 }
