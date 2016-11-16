cb282a364f108903578ca558ece9eaec9b08b3db
Sukora-Stas <stasiksukora@mail.ru>
2016-11-16 14:56:19 +0300

    lab4

--- All_lab/lab4/lab4/lab4.cpp
+++ All_lab/lab4/lab4/lab4.cpp
@@ -1,38 +1,29 @@
 #include <iostream>
-#include <math.h>
+
 using namespace std;
+
 int main()
 {
-	cout << "\n\t       Fourth" << endl;
-	int i, n, **aa, j, m, sum1 = 0;
-	cout << "\t Input N, M : ";
-	cin >> n;
-	m = n;
-
-	aa = new int*[n]; 					// Захват памяти под указатели
-	for (i = 0; i<n; i++)
-		aa[i] = new int[m]; 		// Захват памяти под элементы
-	cout << "\n Input A" << endl;
-	for (i = 0; i<n; i++)
-		for (j = 0; j<m; j++) {
-			cout << "\t a[" << i + 1 << "][" << j + 1 << "] = ";
-			cin >> aa[i][j];
+	int a[4][4];
+	int i, j, S = 0;
+	cout << "Massiv 4x4" << endl;
+	for (i = 1; i <= 4; i++)
+	{
+		for (j = 1; j <= 4; j++)
+		{
+			a[i][j] = 0 + rand() % 10;
+			if (i % 2 == 0){ S = S + a[i][j]; }
 		}
-	cout << "\n Matrix A:" << endl;
-	for (i = 0; i<n; i++) {
-		for (j = 0; j<m; j++)
-			cout << "\t" << aa[i][j];
-		cout << endl;
 	}
-	for (i = 0; i<n; i++) {
-		for (j = 0; j < m; j++)
-			if ((i + j <= n) & (aa[i][j] < 0)) {
-				cout << aa[i][j];
-				sum1 += aa[i][j];
-			}
+	cout << "Mass a[i][j]= \n";
+	for (i = 1; i <= 4; i++)
+	{
+		for (j = 1; j <= 4; j++)
+		{
+			cout << a[i][j] << " ";
+		} cout << endl;
 	}
-	cout << "\n" << sum1;
-	cout << "\n Press any key ... " << endl;
+	cout <<"The sum of the even rows= "<<  S << endl;
 	system("pause");
 	return 0;
 }
