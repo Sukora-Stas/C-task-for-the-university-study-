f3480e2bd52eb100937fd07e87e5bf22cf1ecbb6
Sukora Stas <stasiksukora@mail.ru>
2018-02-27 20:30:22 +0300

    second

--- YIP/YIP/Program.cs
+++ YIP/YIP/Program.cs
@@ -10,6 +10,27 @@ namespace YIP
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("input variable");
+            Console.Write("a: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("b: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("c: ");
+            int c = int.Parse(Console.ReadLine());
+
+
+            double sum = 0;
+
+            sum = ((b + Math.Sqrt(Math.Pow(b, 2) + 4 * a * c)) / 2 * a) - Math.Pow(a, 3) * c + Math.Pow(b, -2);
+
+
+            Console.Write("Otvet: " + sum);
+
+
+
+            Console.ReadLine();
         }
     }
 }
