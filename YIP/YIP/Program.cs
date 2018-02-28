ae53cf348b49eda8f78c386612e966ec7049fcea
Sukora Stas <stasiksukora@mail.ru>
2018-02-28 21:27:53 +0300

    YIP menu

--- YIP/YIP/Program.cs
+++ YIP/YIP/Program.cs
@@ -36,7 +36,26 @@ namespace YIP
                
                 switch (value)
                 {
-                        
+                    case 1:
+                        laba1();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        laba3();
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                            
+                                
                 }
                 
             }
