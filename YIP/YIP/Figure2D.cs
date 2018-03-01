e568047adbe68ed7d3bfbf59c4712948310c16d4
Sukora Stas <stasiksukora@mail.ru>
2018-03-01 21:25:34 +0300

    YIP laba2

---
+++ YIP/YIP/Figure2D.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace YIP
+{
+    public interface Figure2D
+    {
+        void draw();
+
+        void move(int x1, int x2, int y1, int y2);
+
+        void move(int x, int y);
+
+        void scale(int scale);
+
+        void Rotate(double radius);
+    }
+}
--- YIP/YIP/Square.cs
+++ YIP/YIP/Square.cs
@@ -1,6 +1,6 @@
 ﻿namespace YIP
 {
-    public class Square
+    public class Square : Figure2D
     {
         private int x1 { get; set; }
         private int y1 { get; set; }
@@ -23,12 +23,19 @@
             this.y2 = y2;
         }
 
-        public void Scale()
+        public void move(int x, int y)
         {
+            throw new System.NotImplementedException();
         }
 
-        public void Rotate()
+        public void scale(int scale)
         {
+            throw new System.NotImplementedException();
+        }
+
+        public void Rotate(double radius)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
